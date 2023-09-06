2778342b6c8aea74ec0a28c834d5fbac9260e2ea
venivendetta <144264085+venivendetta@users.noreply.github.com>
2023-09-06 14:35:39 -0400

    initial

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("clickMe").addEventListener("click", function() {
+      alert("Button clicked!");
+    });
+  });
