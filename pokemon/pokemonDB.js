90fd29e800f5037e2c2f3869052afd28621c651c
rocksiza <ununivirse1@gmail.com>
2023-09-07 12:08:22 -0400

    a few fixes and such

---
+++ pokemon/pokemonDB.js
@@ -0,0 +1,10 @@
+const pokemonDatabase = {
+    "Pikachu": "path/to/pikachu_sprite.png",
+    "Rhydon": "path/to/rhydon_sprite.png",
+    // ...
+  };
+
+  const locationDatabase = {
+    "Route 5": ["Rhydon", "Pachirisu", "Ledyba", "Oshawott"],
+    // ...
+  };
