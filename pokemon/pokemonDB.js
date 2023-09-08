9ff774a781bd568b6d81c8e70d507b84ea1918a9
rocksiza <ununivirse1@gmail.com>
2023-09-08 10:55:20 -0400

    more pokemon

--- pokemon/pokemonDB.js
+++ pokemon/pokemonDB.js
@@ -1,9 +1,27 @@
 const pokemonDatabase = {
     "Pikachu": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_025_m.gif",
+    "Azurill": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_298.gif",
+    "Whismur": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_293.gif",
+    "Loudred": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_294.gif",
+    "Ninjask": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_291.gif",
+    "Pidgey": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_016.gif",
+    "Spearow": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_021.gif",
+    "Sentret": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_161.gif",
+    "Rattata": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_019_f.gif",
+    "Kakuna": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_014.gif",
+    "Weedle": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_013.gif",
+    "Cyndaquil": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_155.gif",
+    "Totodile": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_158.gif",
+    "Zubat": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_041_f.gif",
+    "Geodude": "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_074.gif",
     // ...
   };
 
   const locationDatabase = {
-    "Route 5": ["Pikachu", "Pikachu"],
+    "Route 29": ["Pidgey", "Sentret"],
+    "Route 46": ["Spearow", "Rattata"],
+    "Route 30": ["Weedle", "Kakuna", "Spearow", "Pidgey"],
+    "Route 31": ["Weedle", "Pidgey"],
+    "Dark Cave": ["Zubat", "Geodude"]
     // ...
   };
